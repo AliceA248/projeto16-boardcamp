786c048a72a2348044b89f8536a6f58caf2a562f
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-05-15 13:32:20 -0300

    ajuste rota

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { createGame, getGames } from "../controllers/games.controller.js";
-import validateSchemaMiddleware from "../middlewares/middleware";
+import validateSchemaMiddleware from "../middlewares/validation.middleware.js";
 import { gameSchema } from "../schemas/schema.js";
 
 
