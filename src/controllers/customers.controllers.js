6ec56e94e157a7ef0dd73ef99786d3b4421edc3f
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2023-05-15 17:01:25 -0300

    ajustes createCustomers

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -28,23 +28,32 @@ function formatCustomerBirthday(customer) {
   return formattedCustomer;
 }
 
-
 export async function createCustomer(req, res) {
   const { name, phone, cpf, birthday } = req.body;
 
+  if (!name) {
+    return res.sendStatus(400);
+  }
+
   const existingCustomer = await db.query('SELECT * FROM customers WHERE cpf = $1', [cpf]);
+
   if (existingCustomer.rowCount > 0) {
     return res.sendStatus(409);
   }
 
-  const customer = await db.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4)', [name, phone, cpf, birthday]);
-  if (customer.rowCount) {
-    return res.sendStatus(201);
+  try {
+    const customer = await db.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4) RETURNING *', [name, phone, cpf, birthday]);
+
+    if (customer.rowCount) {
+      return res.sendStatus(201);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
   }
 
-  res.sendStatus(500);
+  return res.sendStatus(500);
 }
-
 
 
 export async function updateCustomer(req, res) {
--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -1,11 +1,12 @@
 import joi from 'joi'
 
 export const gameSchema = joi.object({
-    name: joi.string().required(),
+    name: joi.string(),
     image: joi.string().uri().required(),
     stockTotal: joi.number().required().min(1),
     pricePerDay: joi.number().required().min(1)
-})
+});
+
 
 export const rentalSchema = joi.object({
     customerId: joi.number().required(),
